6c55fbdb8cad93958b96f71c9934d0be21f00ef4
GodHyum <gusghrlrl123@naver.com>
2018-12-19 01:47:18 +0900

    밝기에 따른 밝기 조절 구현

--- LED_test/LED_test.ino
+++ LED_test/LED_test.ino
@@ -16,34 +16,18 @@ char auth[] = "1084182bd58f49f5817670c4d51d183b";
 char ssid[] = "U+Net1B3B";
 char pass[] = "1000003762";
 
-void setup() {
-  Serial.begin(9600);
-  delay(100);
-
-  Blynk.begin(auth, ssid, pass);
-  delay(100);
-
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  pixels.begin();
-  delay(100);
-  
-  for(int i = 0; i < NUMPIXELS; i++){
-     pixels.setPixelColor(i, pixels.Color(255, 255, 255));
-  }
-  delay(100);
-}
-
-int r = 0, g = 0, b = 0;
-int brightness = 255;
-
+const int MODE_NUM = 3;
 int mymode = 1;
 int toggle = 0;
 int change = 0;
 int power = 0;
 
-const int MODE_NUM = 3;
+// mode 1
+int cds = 0;
+
+// mode 2
+int r = 255, g = 255, b = 255;
+int brightness = 255;
 
 // RGB read
 BLYNK_WRITE(V0){
@@ -81,18 +65,55 @@ void changeMode(){
   Blynk.virtualWrite(V2, mymode);
 }
 
+// change cds_brighness
+int get_cds_brightness(){
+  int cds_brightness = 0;
+  if(200 < cds)
+    cds_brightness = 255;
+  else if(50 < cds)
+    cds_brightness = 100;
+  else
+    cds_brightness = 2;
+  return cds_brightness;
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+
+  Blynk.begin(auth, ssid, pass);
+  delay(100);
+
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  pixels.begin();
+  delay(100);
+}
+
+
 void loop() {
   Blynk.run();
+
+  cds = analogRead(A0);
+  int cds_brightness = get_cds_brightness();
+  Serial.println(cds);
   
   if(power == 1){
     changeMode();
-  
-    if(mymode == 2){
+    
+    if(mymode == 1){
+      for(int i = 0; i < NUMPIXELS; i++){
+         pixels.setPixelColor(i, pixels.Color(r, g, b));
+         pixels.setBrightness(cds_brightness);
+      }
+    }
+    else if(mymode == 2){
       for(int i = 0; i < NUMPIXELS; i++){
          pixels.setPixelColor(i, pixels.Color(r, g, b));
          pixels.setBrightness(brightness);
       }
-      Serial.println(brightness);
     }
   } else{
     for(int i = 0; i < NUMPIXELS; i++)
