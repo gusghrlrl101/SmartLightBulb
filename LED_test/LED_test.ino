d63f42090dd6b1f358251604ac1c35ce1eb5f473
GodHyum <gusghrlrl123@naver.com>
2018-12-19 00:55:24 +0900

    모드 바꾸기 & 보여주기 구현

--- LED_test/LED_test.ino
+++ LED_test/LED_test.ino
@@ -1,58 +1,47 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
-
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
 #define PIN            14
-
-// How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      12
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
 int delayval = 500; // delay for half a second
 
 
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
 char auth[] = "1084182bd58f49f5817670c4d51d183b";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "inhascuba";
-char pass[] = "iss5s019";
-
-int r=0, g=0, b=0;
+char ssid[] = "U+Net1B3B";
+char pass[] = "1000003762";
 
 void setup() {
-  // Debug console
   Serial.begin(9600);
-  delay(500);
+  delay(100);
 
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  Blynk.begin(auth, ssid, pass);
+  delay(100);
+
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
-  pixels.begin(); // This initializes the NeoPixel library.
+  pixels.begin();
+  delay(100);
   
-  Blynk.begin(auth, ssid, pass);
-
-  
+  for(int i = 0; i < NUMPIXELS; i++){
+     pixels.setPixelColor(i, pixels.Color(255, 255, 255));
+  }
+  delay(100);
 }
+
+int r = 0, g = 0, b = 0;
+
+int mymode = 1;
+int toggle = 0;
+int change = 0;
+
+const int MODE_NUM = 3;
 
 BLYNK_WRITE(V0){
   r = param[0].asInt();
@@ -60,19 +49,33 @@ BLYNK_WRITE(V0){
   b = param[2].asInt();
 }
 
+BLYNK_WRITE(V1){
+  toggle = param.asInt();
+}
+
+void changeMode(){
+  if(change == 0){
+    if(toggle == 1)
+      change = 1;
+  } else{
+    if(toggle == 0){
+      change = 0;
+      if (mymode++ == MODE_NUM)
+        mymode = 1;
+    }
+  }
+  Blynk.virtualWrite(V2, mymode);
+}
+
 void loop() {
   Blynk.run();
-  Serial.print(r);
-  Serial.print(", ");
-  Serial.print(g);
-  Serial.print(", ");
-  Serial.print(b);
-  Serial.print("\n");
+  changeMode();
+
+  if(mymode == 2){
+    for(int i = 0; i < NUMPIXELS; i++){
+       pixels.setPixelColor(i, pixels.Color(r, g, b));
+    }
+  }
   
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
- for(int i=0;i<NUMPIXELS;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(r, g, b)); // Moderately bright green color.
- }
-  pixels.show(); // This sends the updated pixel color to the hardware.
+  pixels.show();
 }
